c411867a00dbf3213b110cf4d16a0ee5ad9673be
ADHITYA <av6802@srmist.edu.in>
2026-04-01 08:55:36 +0530

    adhitya added uc2

---
+++ App/src/SingleRoom.java
@@ -0,0 +1,19 @@
+/**
+ * =======================================================================
+ * CLASS - SingleRoom
+ * =======================================================================
+ *
+ * Represents a single room in the hotel.
+ *
+ * @version 2.0
+ */
+public class SingleRoom extends Room {
+
+    /**
+     * Initializes a SingleRoom with
+     * predefined attributes.
+     */
+    public SingleRoom() {
+        super(1, 250, 1500.0);
+    }
+}
