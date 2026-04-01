0b6059a9be42308bc2ca5a809fa943f119bf3f30
ADHITYA <av6802@srmist.edu.in>
2026-04-01 09:02:43 +0530

    adhitya added uc4

---
+++ App/src/UseCase4RoomSearch.java
@@ -0,0 +1,36 @@
+/**
+ * =======================================================================
+ * MAIN CLASS - UseCase4RoomSearch
+ * =======================================================================
+ *
+ * Use Case 4: Room Search & Availability Check
+ *
+ * Description:
+ * This class demonstrates how guests
+ * can view available rooms without
+ * modifying inventory data.
+ *
+ * The system enforces read-only access
+ * by design and usage discipline.
+ *
+ * @version 4.0
+ */
+public class UseCase4RoomSearch {
+
+    /**
+     * Application entry point.
+     *
+     * @param args Command-line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Room Search\n");
+
+        RoomInventory inventory = new RoomInventory();
+        SingleRoom singleRoom = new SingleRoom();
+        DoubleRoom doubleRoom = new DoubleRoom();
+        SuiteRoom suiteRoom = new SuiteRoom();
+
+        RoomSearchService searchService = new RoomSearchService();
+        searchService.searchAvailableRooms(inventory, singleRoom, doubleRoom, suiteRoom);
+    }
+}
